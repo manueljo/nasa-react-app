b0497e4d98e66285d5182038d2e680f836daef1d
manueljo <mikebillyy@gmail.com>
2024-09-08 12:32:46 +0100

    add consitional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Main from "./components/Main";
 import SideBar from "./components/Sidebar";
 import Footer from "./components/Footer";
 
 function App() {
-  const API_KEY = import.meta.env.VITE_NASA_API_KEY
-
   const [showModal, setShowModal] = useState(false);
+  const [data, setData] = useState(null);
 
   const handleModalToggle = () => {
-    setShowModal(!showModal)
-  }
+    setShowModal(!showModal);
+  };
+
+  useEffect(() => {
+    const fetchApiData = async () => {
+      const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json();
+        setData(apiData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchApiData();
+  }, []);
 
   return (
     <>
-      <Main />
-      {showModal && (<SideBar handleModalToggle={handleModalToggle}/>)}
-      <Footer handleModalToggle={handleModalToggle}/>
+      {data ? (
+        <Main />
+      ) : (
+        <div className="loadingState">
+          <i className="fa-solid fa-gear"></i>
+        </div>
+      )}
+      {showModal && <SideBar handleModalToggle={handleModalToggle} />}
+      {data && <Footer handleModalToggle={handleModalToggle} />}
     </>
   );
 }
